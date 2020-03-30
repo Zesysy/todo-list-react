388e3c8ea95acbe8bb316c641aef8dae6824d32e
Sylène Manusset <sylene.manusset@gmail.com>
2020-03-30 18:18:48 +0200

    Initializing a ReactJs application, deleting unnecessary code for the moment, changing the name of the page and adding a favicon

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World !</h1>
+      <h3>Welcome to my todo</h3>
+      <p>In creation ...</p>
+    </>
+  );
+}
+
+export default App;
