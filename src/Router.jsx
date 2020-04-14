291f888f45ca225fcdd982f888a089b9aa310241
Sylène Manusset <sylene.manusset@gmail.com>
2020-04-14 10:34:18 +0200

    [WIP] Retrieving the user id in order to control the rendering of the Router > to follow the display condition on the Navbar with this same id

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 import Home from "./containers/Home";
 import Todos from "./containers/Todos";
 import Login from "./containers/auth/Login";
 import SignUp from "./containers/auth/SignUp";
 
-const Router = () => {
-  return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/todos" component={Todos} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={SignUp} />
-      <Redirect to="/" />
-    </Switch>
-  );
+const Router = ({ loggedIn }) => {
+  let routes;
+
+  loggedIn
+    ? (routes = (
+        <>
+          <Route path="/" exact component={Home} />
+          <Route path="/todos" component={Todos} />
+          <Redirect to="/" />
+        </>
+      ))
+    : (routes = (
+        <>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Redirect to="/" />
+        </>
+      ));
+
+  return <Switch>{routes}</Switch>;
 };
 
-export default Router;
+const mapStateToProps = ({ firebase }) => ({
+  loggedIn: firebase.auth.uid, // Access the user's unique ID
+});
+
+Router.propTypes = {
+  loggedIn: PropTypes.string,
+};
+
+export default connect(mapStateToProps)(Router);
