7d94e467d736d85952b239322be5f81622a8a1da
Sylène Manusset <sylene.manusset@gmail.com>
2020-04-14 11:19:14 +0200

    [DONE] Registration of a user with backup of his data in firestore with protection of todos if not connected

--- src/components/navigation/NavItems.jsx
+++ src/components/navigation/NavItems.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 import NavItem from "./NavItem";
 
@@ -16,30 +17,48 @@ const Ul = styled.ul`
   height: 100%;
 `;
 
-const NavItems = ({ mobile, clicked }) => {
-  const links = [
-    { id: 0, path: "/", placeholder: "Accueil" },
-    { id: 1, path: "/todos", placeholder: "Todos" },
-    { id: 2, path: "/login", placeholder: "Connexion" },
-    { id: 2, path: "/signup", placeholder: "Inscription" },
-  ];
+const NavItems = ({ mobile, clicked, loggedId }) => {
+  // TODO : refactorring ?  const links = [
+  //   { id: 0, path: "/", placeholder: "Accueil" },
+  //   { id: 1, path: "/todos", placeholder: "Todos" },
+  //   { id: 2, path: "/login", placeholder: "Connexion" },
+  //   { id: 3, path: "/signup", placeholder: "Inscription" },
+  // ];
 
-  return (
-    <Nav mobile={mobile}>
-      <Ul mobile={mobile}>
-        {links.map(({ path, placeholder }, key) => (
-          <NavItem mobile={mobile} clicked={clicked} link={path} key={key}>
-            {placeholder}
+  let links;
+  loggedId
+    ? (links = (
+        <Ul mobile={mobile}>
+          <NavItem mobile={mobile} clicked={clicked} link="/">
+            Accueil
           </NavItem>
-        ))}
-      </Ul>
-    </Nav>
-  );
+          <NavItem mobile={mobile} clicked={clicked} link="/todos">
+            Todos
+          </NavItem>
+        </Ul>
+      ))
+    : (links = (
+        <Ul mobile={mobile}>
+          <NavItem mobile={mobile} clicked={clicked} link="/login">
+            Connexion
+          </NavItem>
+          <NavItem mobile={mobile} clicked={clicked} link="/signup">
+            Inscription
+          </NavItem>
+        </Ul>
+      ));
+
+  return <Nav mobile={mobile}>{links}</Nav>;
 };
+
+const mapStateToProps = ({ firebase }) => ({
+  loggedId: firebase.auth.uid,
+});
 
 NavItems.propTypes = {
   mobile: PropTypes.bool,
   clicked: PropTypes.func,
+  loggedId: PropTypes.string,
 };
 
-export default NavItems;
+export default connect(mapStateToProps)(NavItems);
